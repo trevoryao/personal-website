2b5a992f7510c6d1b90b76e25a06f693ea5bff12
Trevor Yao <trevoryao@gmail.com>
2021-03-25 19:02:44 -0400

    icon links

--- src/Footer.js
+++ src/Footer.js
@@ -1,16 +1,26 @@
+import emailIcon from './icons/email-icon.png'
+import githubIcon from './icons/github-icon.png'
+import linkedinIcon from './icons/linkedin-icon.png'
+
 import './Footer.css'
 
 function RenderFooter() {
   return (
     <div className="Footer">
       <div className="Contact">
-        <a href="mailto:trevor.yao@uwaterloo.ca">Email</a>
+        <a href="mailto:trevor.yao@uwaterloo.ca">
+          <img src={emailIcon} alt="Email" />
+        </a>
       </div>
       <div className="Contact">
-        <a href="https://github.com/trevoryao" target="_blank" rel="noreferrer">GitHub</a>
+        <a href="https://github.com/trevoryao" target="_blank" rel="noreferrer">
+          <img src={githubIcon} alt="GitHub" />
+        </a>
       </div>
       <div className="Contact">
-        <a href="https://www.linkedin.com/in/trevoryao/" target="_blank" rel="noreferrer">LinkedIn</a>
+        <a href="https://www.linkedin.com/in/trevoryao/" target="_blank" rel="noreferrer">
+          <img src={linkedinIcon} alt="LinkedIn" />
+        </a>
       </div>
       <div className="Copyright">
         &#169; Trevor J. Yao, 2021.

9e1720f2f38be7d3d741dd5eec8ea519f9c70f6d
Trevor Yao <trevoryao@gmail.com>
2021-03-25 22:45:53 -0400

    add photo of self

--- src/About.js
+++ src/About.js
@@ -1,22 +1,51 @@
+import { BrowserView, MobileView } from "react-device-detect";
+
+import personalPhoto from './img/personal-photo.jpg';
+
 function RenderAbout() {
   return (
     <div>
       <h2 id="about">About Me</h2>
-      <p>
-        Hey, I'm Trevor &mdash; a software developer and student at the University of Waterloo.
-        Currently, I'm working at Sun Life Financial as a DevOps Engineer, and will be transitioning
-        to a Data &amp; Analytics (Big Data) development role in May 2021.
-        I've worked in the past as a software developer, and that is my primary/preferred role, but I am
-        flexible &mdash; I excel at all development roles.
-        My current passions are low-powered embedded systems &amp; machine learning. I believe that
-        combining these two fields will be the future of the computer science industry.
-      </p>
-      <p>
-        Outside of work &amp; school, some of my passions include football &mdash; I'm a childhood Manchester United
-        fan, and I build custom mechanical keyboards in my spare time (Feel free to ask me more
-        about these!). These hobbies also inspire some of my more casual <a href="#projects">projects</a>.
-        I'm also an ex-musician and pilot, and I enjoy cooking &amp; exploring nature during my down time.
-      </p>
+      {/* <MobileView> */}
+        <p>
+          Hey, I'm Trevor &mdash; a software developer and student at the University of Waterloo.
+          Currently, I'm working at Sun Life Financial as a DevOps Engineer, and will be transitioning
+          to a Data &amp; Analytics (Big Data) development role in May 2021.
+          I've worked in the past as a software developer, and that is my primary/preferred role, but I am
+          flexible &mdash; I excel at all development roles.
+          My current passions are low-powered embedded systems &amp; machine learning. I believe that
+          combining these two fields will be the future of the computer science industry.
+        </p>
+        <img src={personalPhoto} alt="Trevor J. Yao" />
+        <p>
+          Outside of work &amp; school, some of my passions include football &mdash; I'm a childhood Manchester United
+          fan, and I build custom mechanical keyboards in my spare time (Feel free to ask me more
+          about these!). These hobbies also inspire some of my more casual <a href="#projects">projects</a>.
+          I'm also an ex-musician and pilot, and I enjoy cooking &amp; exploring nature during my down time.
+        </p>
+      {/* </MobileView>
+      <BrowserView>
+        <div className="FlexImg">
+          <div className="TextSection">
+            <p>
+              Hey, I'm Trevor &mdash; a software developer and student at the University of Waterloo.
+              Currently, I'm working at Sun Life Financial as a DevOps Engineer, and will be transitioning
+              to a Data &amp; Analytics (Big Data) development role in May 2021.
+              I've worked in the past as a software developer, and that is my primary/preferred role, but I am
+              flexible &mdash; I excel at all development roles.
+              My current passions are low-powered embedded systems &amp; machine learning. I believe that
+              combining these two fields will be the future of the computer science industry.
+            </p>
+            <p>
+              Outside of work &amp; school, some of my passions include football &mdash; I'm a childhood Manchester United
+              fan, and I build custom mechanical keyboards in my spare time (Feel free to ask me more
+              about these!). These hobbies also inspire some of my more casual <a href="#projects">projects</a>.
+              I'm also an ex-musician and pilot, and I enjoy cooking &amp; exploring nature during my down time.
+            </p>
+          </div>
+          <img src={personalPhoto} alt="Trevor J. Yao" style={{ display: 'table-cell' }} />
+        </div>
+      </BrowserView> */}
     </div>
   )
 }

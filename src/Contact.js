1fff8b67c44faebda2d5ba49edf0b7847a7236fd
Trevor Yao <trevoryao@gmail.com>
2023-12-19 11:25:35 -0500

    fix resume hyperlink bug

--- src/Contact.js
+++ src/Contact.js
@@ -9,7 +9,7 @@ function RenderContact() {
         my <a href="https://github.com/trevoryao" target="_blank" rel="noreferrer">GitHub</a>,
         and feel free to reach out
         on <a href="https://www.linkedin.com/in/trevoryao/" target="_blank" rel="noreferrer">LinkedIn</a> as
-        well. Also, feel free to download my <a href="resume.pdf" target="_blank" rel="noreferrer">resume</a>.
+        well. Also, feel free to download my <a href="files/resume.pdf" target="_blank" rel="noreferrer">resume</a>.
       </p>
     </div>
   )
